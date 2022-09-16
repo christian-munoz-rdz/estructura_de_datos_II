131a47447ccd91df5c18a14e787df5e604d95022
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-09-16 16:35:00 -0500

    Documentacion

--- programa_01_lista_simple/List.h
+++ programa_01_lista_simple/List.h
@@ -2,13 +2,16 @@
 #include<fstream>
 #include<string>
 
+// Clase lista solo tiene un atributo: un puntero al primer nodo de la lista
 class List{
 private:
     Node* Head;
 
 public:
+    // Constructor de la clase
     List();
 
+    // Metodos de la clase
     bool Empty();
     void Insert(Node*, Student&);
     void Delete(string);
@@ -26,69 +29,75 @@ public:
 
 };
 
+// Constructor de la clase lista no recibe parametros
 List::List(){ 
-	Head = NULL;
+	Head = NULL; // Inicializamos el puntero a NULL
 }
 
+// Metodo que retorna true si la lista esta vacia y false si no lo esta
 bool List::Empty(){
     return Head==NULL;
 }
 
+// Metodo que recibe un puntero a nodo y un estudiante y lo inserta en la lista
 void List::Insert(Node* n, Student& s){
-    Node* aux= new Node();
-    aux->set_Data(s);
+    Node* aux= new Node(); // Reservamos memoria para el nodo auxiliar
+    aux->set_Data(s); // Guardamos el estudiante en el nodo auxiliar
     
-    if(n==NULL){
-        aux->set_Next(Head);
+    if(n==NULL){ // Si el puntero recibido es NULL
+        aux->set_Next(Head); // El puntero Next del nodo auxiliar apunta al primer nodo de la lista
         if(Head!=NULL){
-            Head->set_Prev(aux);
+            Head->set_Prev(aux); // El puntero Previous del primer nodo de la lista apunta al nodo auxiliar
         }
-        Head=aux;
+        Head=aux; // El puntero Head apunta al nodo auxiliar
     }
-    else{
-        aux->set_Prev(n);
-        aux->set_Next(n->get_Next());
-        if(n->get_Next()!=NULL){
-            n->get_Next()->set_Prev(aux);
+    else{ // Si el puntero recibido no es NULL
+        aux->set_Prev(n); // El puntero Previous del nodo auxiliar apunta al nodo recibido
+        aux->set_Next(n->get_Next()); // El puntero Next del nodo auxiliar apunta al nodo siguiente al nodo recibido
+        if(n->get_Next()!=NULL){ // Si el nodo siguiente al nodo recibido no es NULL
+            n->get_Next()->set_Prev(aux); // El puntero Previous del nodo siguiente al nodo recibido apunta al nodo auxiliar
         }
-        n->set_Next(aux);
+        n->set_Next(aux); // El puntero Next del nodo recibido apunta al nodo auxiliar
     }
 
 }
 
+// Metodo que recibe un string y lo busca en la lista, si lo encuentra lo elimina
 void List::Delete(string code){
-    Node* n= Search(code);
+    Node* n= Search(code); // Buscamos el nodo con el codigo recibido
     
     if(n==NULL){
-        cout << "No se encontro el estudiante..." << endl;
+        cout << "No se encontro el estudiante..." << endl; // Mensaje de error si no se encuentra el estudiante
     }
     else{
         Node* aux=n;
     
-        if(n->get_Prev()!=NULL){
-            n->get_Prev()->set_Next(n->get_Next()); 
+        if(n->get_Prev()!=NULL){ // Si el nodo anterior no es NULL
+            n->get_Prev()->set_Next(n->get_Next()); // Asignamos el siguiente nodo del nodo anterior al siguiente nodo del nodo a eliminar
         }
     
-        if(n->get_Next()!=NULL){
-            n->get_Next()->set_Prev(n->get_Prev());
+        if(n->get_Next()!=NULL){ // Si el nodo siguiente no es NULL
+            n->get_Next()->set_Prev(n->get_Prev()); // Asignamos el nodo anterior del nodo siguiente al nodo anterior del nodo a eliminar
         }
         if(n==Head){
-            Head=n->get_Next();
+            Head=n->get_Next(); // Si el nodo a eliminar es el primero de la lista, asignamos el siguiente nodo como el primero
         }
 
-        delete aux;
+        delete aux; // Eliminamos el nodo
     }
 
 }
 
+// Metodo que retorna un string con los datos de todos los estudiantes de la lista
 string List::Show_list(){ 
     Node* n = Head;
-    string text = "";
+    string text = ""; // Variable que almacenara el string a retornar
     
     if(Empty()){ 
-        return "La lista esta vacia...";
+        return "La lista esta vacia..."; // Mensaje de error si la lista esta vacia
     }
     
+    // Recorremos la lista y concatenamos los datos de cada estudiante
     while(n!=NULL){ 
         text += n->get_Data_show()+"\n"; 
         n=n->get_Next(); 
@@ -96,98 +105,119 @@ string List::Show_list(){
     return text;
 }
 
+// Metodo que recibe un string y lo busca en la lista, si lo encuentra retorna un string con los datos del estudiante
 string List::Show_Data(string code){ 
-   Node* n = Search(code);
+   Node* n = Search(code); // Buscamos el nodo con el codigo recibido
 
-    if(n==NULL){ 
+    if(n==NULL){ // Mensaje de error si no se encuentra el estudiante
        return "No existe el estudiante";
    }
-    return n->get_Data_show(); 
+    return n->get_Data_show(); // Retorna los datos del estudiante
 }
 
+// Metodo que recibe un string y lo busca en la lista, si lo encuentra retorna un puntero al nodo donde se encuentra
 Node* List::Search(string code){ 
     Node* aux=Head;
 
     while(aux!=NULL){ 
-        if(aux->Record.get_Code()==code){
-            return aux; 
+        if(aux->Record.get_Code()==code){ //si el codigo del estudiante es igual al codigo que se busca
+            return aux; // Retorna el puntero al nodo donde se encuentra el estudiante
         }
         aux= aux->get_Next();
     }
-    return NULL; 
+    return NULL; // Retorna NULL si no se encuentra el estudiante
 }
 
+// Metodo que elimina todos los nodos de la lista
 void List::Clear(){
     Node* aux;
     
-    int count = 0;
+    int count = 0; // Contador de nodos eliminados
     while(Head!=NULL){
         aux=Head;
-        Head=Head->get_Next();
-        delete aux;
+        Head=Head->get_Next(); // Reasignamos la cabeza de la lista
+        delete aux; // Eliminamos el nodo auxiliar
         count++; 
     }
     cout<<"Lista completamente vacia..."<<endl;
-    cout<<"Se eliminaron "<<count<<" elementos..."<<endl; 
+    cout<<"Se eliminaron "<<count<<" elementos..."<<endl; // mostramos el numero de nodos eliminados 
 }
 
+// Metodo que retorna un puntero al ultimo nodo de la lista
 Node* List::Last(){
 
+    // Si la lista esta vacia retornamos NULL
     if(Head==NULL){
         return Head;
     }
     
+    // Creamos una variable auxiliar que apunte al primer nodo de la lista
     Node* aux=Head;
 
+    // Recorremos la lista hasta llegar al ultimo nodo
     while(aux->get_Next()!=NULL){
         aux=aux->get_Next(); 
     }
     return aux; 
 }
 
+// Metodo que retorna un puntero al primer nodo de la lista
 Node* List::First(){
     return Head;
 }
 
+// Metodo que guarda los datos de la lista en un archivo
 void List::Save(){
 
     if(Empty())
-        cout<<"La lista esta vacia"<<endl;                               
+        cout<<"La lista esta vacia"<<endl;//                 
     
     Node* n=Head;
 
+    // Creamos un string para guardar los datos de los estudiantes
     string text;
 
+    // Recorrer la lista y guardar los datos de los estudiantes en el string
     while(n!=NULL){
         text += n->get_Data_save();
         n=n->get_Next();
     }
 
+    // Creamos un objeto de la clase ofstream para escribir en el archivo
     ofstream file;
 
+    // Abrimos el archivo en modo escritura
     file.open("data.txt",ios::out);
     
+    // Si el archivo no se pudo abrir mostramos un mensaje de error
     if(file.fail()){
         cout<<"No se pudo abrir el archivo"<<endl;
         exit(1);
     }
 
+    // Escribimos los datos en el archivo
     file<<text;
+    // Cerramos el archivo
     file.close(); 
 }
 
+// Metodo que recibe un string y lo divide en campos para crear un estudiante
 void List::Fields(string& record){
-    int i=0;
+    int i=0; // Inicializamos un iterador para recorrer el string
     
+    // Guardamos cada campo en un string
     string Name="";
     
+    // Recorremos el string hasta encontrar un caracter de separacion
     while(record[i]!='|'){
         Name += record[i];
         i++;
     }
 
+    // Incrementamos el iterador para saltar el caracter de separacion
     i++;
 
+    //Repetimos el proceso para los demas campos
     string Code="";
 
     while(record[i]!='|'){
@@ -213,45 +243,53 @@ void List::Fields(string& record){
         i++;
     }
 
+    // Creamos un estudiante con los datos obtenidos
     Student s(Name,Code,Career,Grade);
 
+    // Insertamos el estudiante en la lista
     Insert(Last(),s);
 }
 
+// Metodo que carga los datos de la lista desde un archivo
 void List::Load(){
+
     Node* aux=Head;
 
+    // Creamos un objeto de la clase ifstream para leer el archivo
     ifstream file;
 
+    // Abrimos el archivo en modo lectura
     file.open("data.txt",ios::in);
 
+    // Si podemos abrir el archivo leemos los datos
     if(file.is_open()){
         
-        file.seekg(0,file.end); 
-        int length=file.tellg(); 
-        file.seekg(0,file.beg); 
+        file.seekg(0,file.end); // Nos posicionamos al final del archivo 
+        int length=file.tellg(); // Obtenemos la longitud del archivo
+        file.seekg(0,file.beg); // Nos posicionamos al inicio del archivo
 
-        char *buffer = new char[length]; 
+        char *buffer = new char[length]; // Creamos un buffer para guardar los datos del archivo
 
-        file.read(buffer,length); 
+        file.read(buffer,length);  // Leemos los datos del archivo y los guardamos en el buffer
         
-        int i=0; 
-        string record=""; 
-    
+        int i=0; // Inicializamos un iterador para recorrer el buffer
+        string record=""; // Creamos un string para guardar los datos de cada estudiante
+
+        // Recorremos el buffer hasta encontrar un caracter de separacion
         while(i<length){ 
             if(buffer[i]!='*'){ 
-                record+=buffer[i]; 
+                record+=buffer[i]; // Guardamos los datos en el strig de estudiante
             }
             else{ 
-                Fields(record); 
-                record=""; 
+                Fields(record); // Creamos un estudiante con los datos del string si encontramos un caracter de separacion
+                record=""; // Reiniciamos el string de estudiante
             }
-            i++; 
+            i++; // Incrementamos el iterador
         }
-        file.close(); 
+        file.close(); // Cerramos el archivo
     }
     else{
-        return; 
+        return; // Si no se pudo abrir el archivo retornamos
     }
 }
 
--- programa_01_lista_simple/main.cpp
+++ programa_01_lista_simple/main.cpp
@@ -6,8 +6,10 @@
 using namespace std;
 
 int main(){
+// Creamos un objeto de la clase lista
     List l1;
-        
+
+// Cargamos los datos de los estudiantes desde el archivo si es que existe
     l1.Load();
     int opc;
  
@@ -28,7 +30,7 @@ do{
     
 
     switch (opc){
-        case 1:{
+        case 1:{ // Pedimos los datos del estudiante y lo insertamos en la lista
                 string Name,Code,Career,Grade;
                 cout<< "\n---Registro de estudiante---" << endl;
                 cout << "Ingresa el nombre: "; getline(cin,Name);
@@ -39,11 +41,11 @@ do{
                 l1.Insert(l1.Last(),s);
                 break;
                 }   
-        case 2:
+        case 2: // Mostramos la lista completa
                 cout<<"\n---Lista completa--- "<<endl<<endl;
                 cout<< l1.Show_list()<<endl<<endl;
                 break;
-        case 3:{
+        case 3:{ // Pedimos el codigo del estudiante y lo eliminamos de la lista
                 string code;
                 cout<< "\n---Eliminacion de estudiante---" << endl; 
                 cout<<"Ingresa de favor el codigo del alumno: ";cin>>code;
@@ -51,7 +53,7 @@ do{
                 cin.ignore();
                 break;
                 }
-        case 4:{
+        case 4:{ // Pedimos el codigo del estudiante y mostramos sus datos
                 string code;
                 cout<< "\n---Mostrar estudiante---" << endl; 
                 cout<<"Ingresa de favor el codigo del alumno: ";cin>>code;
@@ -59,38 +61,38 @@ do{
                 cin.ignore();
                 break;
                 }
-        case 5:
+        case 5: // Vaciamos la lista
                 cout<< "\n---Eliminacion de lista---" << endl;
                 l1.Clear();
                 break;
-        case 6: 
+        case 6: // Mostramos el primer estudiante de la lista
                 cout<< "\n---Mostrando primera posicion---" << endl;
                 if(l1.Empty())
-                    cout<<"La lista esta vacia"<<endl;
+                    cout<<"La lista esta vacia"<<endl; // Si la lista esta vacia mostramos un mensaje
                 else
                     cout<<"\n"<<l1.First()->get_Data_show()<<endl;
                 break;
-        case 7: 
+        case 7: // Mostramos el ultimo estudiante de la lista
                 cout<< "\n---Mostrando ultima posicion---" << endl; 
                 if(l1.Empty())
-                    cout<<"La lista esta vacia"<<endl;
+                    cout<<"La lista esta vacia"<<endl; // Si la lista esta vacia mostramos un mensaje
                 else
                     cout<<l1.Last()->get_Data_show()<<endl;
                 break;
-        case 8:
+        case 8:// Guardamos los datos de la lista en el archivo
                 cout<< "\n---Guardando datos---" << endl;
                 l1.Save();
                 break;
-        case 0:
+        case 0:// Salimos del programa
                 cout<<"\n---VUELVA PRONTO---"<<endl;
                 break;
-        default:
+        default:// Si el usuario ingresa una opcion no valida mostramos un mensaje
                 cout<<"\n---OPCION NO VALIDA---"<<endl;
                 break;
     } 
     cout<<"\nEnter para continuar..."<<endl;
     cin.ignore();
-    system("clear");
+    system("clear"); // Limpiamos la pantalla
 }while (opc!=0); 
     return 0;
 }
